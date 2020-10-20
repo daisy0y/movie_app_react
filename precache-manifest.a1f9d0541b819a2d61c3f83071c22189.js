0126c4e0e6ffd0485c4c306244cfbfbb1010790f
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-10-20 23:44:25 +0900

    Updates

---
+++ precache-manifest.a1f9d0541b819a2d61c3f83071c22189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4492c58289df91e249b91f0a128c32ee",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "3cb4d29ef8cfdb4ea5b1",
+    "url": "/movie_app_react/static/css/main.5b04a7d1.chunk.css"
+  },
+  {
+    "revision": "ecde05f7bdcfd9cf9091",
+    "url": "/movie_app_react/static/js/2.3cc015df.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_app_react/static/js/2.3cc015df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb4d29ef8cfdb4ea5b1",
+    "url": "/movie_app_react/static/js/main.1fb763b4.chunk.js"
+  },
+  {
+    "revision": "bc0b3f28a4ea27a8f900",
+    "url": "/movie_app_react/static/js/runtime-main.1e79b605.js"
+  }
+]);
